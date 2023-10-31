860a59b852f5c70637c1e00e4ce98d0a40a377f5
Nuvem333 <146393220+Nuvem333@users.noreply.github.com>
2023-10-30 21:15:35 -0300

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE games, teams")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+ do
+  if [[ $WINNER != 'winner' ]]
+    then
+       #get winner id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+         #if not found
+      if [[ -z $WINNER_ID ]]
+        then
+          #insert winner team
+         INSERT_WINNER_RES=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+          if [[ $INSERT_WINNER_RES == 'INSERT 0 1' ]]
+            then
+              echo Inserted into names $WINNER
+          fi  
+           #get new winner id 
+           WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+      fi  
+
+      #get opponent id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+      #if not found
+      if [[ -z $OPPONENT_ID ]] 
+        then 
+        #insert opponent team
+        INSERT_OPPONENT_RES=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')") 
+         if [[ $INSERT_OPPONENT_RES == 'INSERT 0 1' ]]
+            then
+              echo Inserted into names $OPPONENT
+          fi   
+        # get new opponent id
+        OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+      fi
+
+      #insert into games
+      INSERT_GAMES_RES=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+       if [[ $INSERT_GAMES_RES == "INSERT 0 1" ]]
+    then
+      echo Inserted into games, $ROUND $WINNER x $OPPONENT: $WINNER_GOALS  $OPPONENT_GOALS
+    fi
+  fi  
+ done
+
